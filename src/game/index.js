0e4ee9c411d4d0cdbbf79ff36b06afa56758a3c6
chrissheppard <chrissheppard@fullsixuk.com>
2015-01-14 14:47:49 +0000

    feat(index): add index to view game

---
+++ src/game/index.js
@@ -0,0 +1,26 @@
+// Prevent fuzzy scaling on pixels
+PIXI.scaleModes.DEFAULT = PIXI.scaleModes.NEAREST;
+
+import GFX from "../engine/GFX";
+import Game from "../engine/Game";
+import AssetManager from "../engine/AssetManager";
+import SceneManager from "../engine/SceneManager";
+
+import IntroScene from "./scene/IntroScene";
+import MainMenuScene from "./scene/MainMenuScene";
+
+// Create a renderer
+GFX.createRenderer(800, 600);
+
+// Create scenes
+SceneManager.createScene("intro", IntroScene);
+SceneManager.createScene("main-menu", MainMenuScene);
+SceneManager.goToScene("intro");
+
+// Load assets
+AssetManager.addImage("logo_small.png");
+AssetManager.load();
+
+// Create and run the game
+var game = new Game();
+game.run();
