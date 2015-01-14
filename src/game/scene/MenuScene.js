01a8e43739f32ed65e4ac6a8566d516349f8117a
chrissheppard <chrissheppard@fullsixuk.com>
2015-01-14 13:52:17 +0000

    feat(menuscene): create MenuScene

---
+++ src/game/scene/MenuScene.js
@@ -0,0 +1,23 @@
+import Scene from "../../engine/Scene";
+
+
+/**
+ * @class
+ * @extends Scene
+ */
+class MenuScene extends Scene {
+    /**
+     * @constructor
+     */
+    constructor() {
+        super(0x000000);
+    }
+
+    init() {
+    }
+
+    update(dt) {
+    }
+}
+
+export default MenuScene;
