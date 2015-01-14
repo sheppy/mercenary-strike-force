6a9cc34b674d752d26de53788959a8a9a51dcdf3
chrissheppard <chrissheppard@fullsixuk.com>
2015-01-14 11:30:29 +0000

    feat(scene): create Scene class
    Scene class will be used for each scene / screen in the game.

---
+++ src/engine/Scene.js
@@ -0,0 +1,31 @@
+export default
+class Scene extends PIXI.Stage {
+    constructor(background) {
+        super(background);
+
+        this.updateCB = null;
+        this.paused = false;
+    }
+
+    onUpdate(updateCB = null) {
+        this.updateCB = updateCB;
+    }
+
+    update() {
+        if (typeof this.updateCB === "function") {
+            this.updateCB();
+        }
+    }
+
+    pause() {
+        this.paused = true;
+    }
+
+    resume() {
+        this.paused = false;
+    }
+
+    isPaused() {
+        return this.paused;
+    }
+}
