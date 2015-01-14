915806e5e6c8d1de558e86abb6a627fc85155245
chrissheppard <chrissheppard@fullsixuk.com>
2015-01-14 11:28:35 +0000

    feat(game): create Game class
    Game class will be used for the main game loop.

---
+++ src/engine/Game.js
@@ -0,0 +1,33 @@
+export default
+class Game {
+    constructor() {
+        this.skipTicks = 1000 / 120;
+        this.nextUpdateTick = Date.now();
+    }
+
+    update(dt) {
+        // TODO: Get current scene & update
+    }
+
+    render() {
+        // TODO: Get current scene & render
+    }
+
+    run() {
+        window.requestAnimationFrame(this.run.bind(this));
+
+        var loops = 0;
+
+        while(Date.now() > this.nextUpdateTick) {
+            // TODO: Pass dt
+            this.update();
+            this.nextUpdateTick += this.skipTicks;
+            loops++;
+        }
+
+        // If we actually updated anything
+        if (loops) {
+            this.render();
+        }
+    }
+}
